c5d185cea732dc772c3b8d824761708fb7e0297d
Shen Zhou <90119403+gettingthestars@users.noreply.github.com>
2024-07-28 19:59:36 -0400

    Update python_codes_structured_data.py

--- python_codes_structured_data.py
+++ python_codes_structured_data.py
@@ -1,5 +1,12 @@
 import os
-os.getcwd()#get current working directory
-os.chdir("/Users/shenzhou/Desktop/)#change the current working directory to specified new path
-os.listdir()#list all files in the current working directory
+import pandas as pd
+from sklearn.preprocessing import StandardScaler import numpy as np
 
+os.getcwd() #get current working directory
+os.chdir("/Users/shenzhou/Desktop/) #change the current working directory to specified new path
+os.listdir() #list all files in the current working directory
+
+df=pd.read_csv("tumor_cells.csv",header=None) # tumor_cells.csv is an example file
+
+pd.set_option('display.max_columns', 10) # show 10 columns
+pd.set_option('display.max_columns', None) # show all columns
