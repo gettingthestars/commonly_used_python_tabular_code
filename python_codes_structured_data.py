b361eaef16d425a075d08e9c4a1d3abc3cb74563
Shen Zhou <90119403+gettingthestars@users.noreply.github.com>
2024-07-28 19:55:30 -0400

    Update python_codes_structured_data.py

--- python_codes_structured_data.py
+++ python_codes_structured_data.py
@@ -1,3 +1,5 @@
 import os
-os.getcwd()
+os.getcwd()#get current working directory
+os.chdir("/Users/shenzhou/Desktop/)#change the current working directory to specified new path
+os.listdir()#list all files in the current working directory
 
