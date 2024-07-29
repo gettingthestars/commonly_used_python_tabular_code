f64b24235f6cd298f46c49434d8347a8387ded98
Shen Zhou <90119403+gettingthestars@users.noreply.github.com>
2024-07-28 21:37:38 -0400

    Update python_codes_structured_data.py

--- python_codes_structured_data.py
+++ python_codes_structured_data.py
@@ -6,9 +6,11 @@ os.getcwd() #get current working directory
 os.chdir("/Users/shenzhou/Desktop/) #change the current working directory to specified new path
 os.listdir() #list all files in the current working directory
 
-df=pd.read_csv("tumor_cells.csv",header=None) # tumor_cells.csv is an example file
-
 pd.set_option('display.max_columns', 10) # show 10 columns
 pd.set_option('display.max_columns', None) # show all columns
 pd.set_option('display.max_rows', 10) # show 10 rows
 pd.set_option('display.max_rows', None) # show all rows
+df=pd.read_csv("tumor_cells.csv",header=None) # tumor_cells.csv is an example file
+
+
+
